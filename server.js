c52190c9ce3ab89114b573237c5ab83d0ff2cb84
LautaroTiamat <lautarotiamat@gmail.com>
2021-02-02 00:40:26 -0300

    lastChanges

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const puppeteer = require("puppeteer");
 
-const message = "GG";
+const Message = 'Wenas noshes x10';
+const ChatType = ''; // 'Group'
+const ChatName = 'Leandro'; // 'Lolcito izi'
+const RepeatNumber = 10;
 
-async function scrape(url) {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(url);
+const WspScrape = async (chatType, chatName, message, repeatNumber) => {
+    console.log("Scrape started...");
+    const URL = "https://web.whatsapp.com";
+    const Browser = await puppeteer.launch({ headless: false, userDataDir: './data' });
+    const Page = await Browser.newPage();
+    await Page.goto(URL);
+    const Content = chatType === 'Group' ? `div [title='${chatName}']` : `span [title='${chatName}']`;
+    await Page.waitForSelector(Content);
+    const Target = await Page.$(Content);
+    await Target.click().then(() => {
+        setTimeout(async () => {
+            for(let i = 0; i < repeatNumber; i++){
+                await Page.keyboard.type(message);
+                await Page.keyboard.press("Enter");
+            };
+        }, 5000);
+    });
+};
 
-    /* Persona */
-    await page.waitForSelector("span [title='Leandro']");
-    const target = await page.$("span [title='Leandro']");
-
-    /* Grupo */
-    //await page.waitForSelector("div [title='Team Lag Mental']");
-    //const target = await page.$("div [title='Team Lag Mental']");
-
-    await target.click();
-
-    for(let i = 0; i < 5; i++){
-        await page.keyboard.type(message);
-        await page.keyboard.press("Enter");
-    }
-}
-
-scrape("https://web.whatsapp.com");
+WspScrape(ChatType, ChatName, Message, RepeatNumber).then(() => {
+    console.log('Scrape finished.');
+});
